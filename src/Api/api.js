14a1e7c9a8a1cee0ac3dd2a24cb599a396a1266e
Ali raza <chatwithaliraza@gmail.com>
2026-02-25 12:19:04 +0500

    Update API baseURL to deployed backend and image URLs

--- src/Api/api.js
+++ src/Api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://salon-backend-pdcn.vercel.app/api", 
-  withCredentials: true, 
+  baseURL: `${BASE_URL}/api`,
+  withCredentials: true,
 });
 
 export default API;
